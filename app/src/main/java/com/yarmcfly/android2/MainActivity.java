a19c58b4f5458dd5402eb7292aac51f09cb9cc77
Yaroslav <yarmcfly@gmail.com>
2019-03-25 19:22:38 +0200

    dz_02

--- app/src/main/java/com/yarmcfly/android2/MainActivity.java
+++ app/src/main/java/com/yarmcfly/android2/MainActivity.java
@@ -1,7 +1,14 @@
 package com.yarmcfly.android2;
 
+
+
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.widget.Button;
+import android.widget.EditText;
+
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +16,32 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
+
+        EditText etName = findViewById(R.id.etName);
+        final Button btnAdd = findViewById(R.id.btnAdd);
+        btnAdd.setEnabled(false);
+
+
+
+        etName.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+                String text = s.toString();
+                btnAdd.setEnabled(!text.isEmpty());
+                if(!text.isEmpty())
+                btnAdd.setTextColor(getResources().getColor(R.color.colorPrimary));
+                else btnAdd.setTextColor(getResources().getColor(R.color.Dark));
+            }
+        });
+        }
 }
